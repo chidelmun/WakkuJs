2925912be519cfcff6014becfe3e3ca56579934d
Delveri Munang <chickdelveri@gmail.com>
2017-07-18 18:57:02 -0400

    Fixed Javascript UI component

--- src/wakkuforms.js
+++ src/wakkuforms.js
@@ -1,3 +1,7 @@
+/*
+	Author  : Delveri Chick
+	Done  	: 2017
+*/
 function createForms(){
 		
 
@@ -20,17 +24,20 @@ function createForms(){
 		var forms  = document.getElementsByClassName('wakkuform');
 			for(var i=0 ; i< forms.length ; ++i){
 				if (forms[i].getAttribute("data-form-type") == "login" || "signin") {
-					var username = document.createElement("input");
-					username.setAttribute('placeholder', 'Username or Email');
-					var password = document.createElement("input");
-					password.setAttribute('placeholder', 'Your password');
-					var button = document.createElement("button");
-					button.setAttribute('type', 'submit');
-					button.setAttribute('class', 'btn btn-success');
-					button.innerHTML = "Login";
-					forms[i].appendChild(username);
-					forms[i].appendChild(password);
-					forms[i].appendChild(button);
+
+					var loginform = '<form class="form-inline">  <div class="form-group">    <label class="sr-only" for="email">Email address:</label>    <input type="email" class="form-control" id="email">  </div>  <div class="form-group">    <label class="sr-only" for="pwd">Password:</label>    <input type="password" class="form-control" id="pwd">  </div>  <div class="checkbox">    <label><input type="checkbox"> Remember me</label>  </div>  <button type="submit" class="btn btn-default">Submit</button></form>';
+					// var username = document.createElement("input");
+					// username.setAttribute('placeholder', 'Username or Email');
+					// var password = document.createElement("input");
+					// password.setAttribute('placeholder', 'Your password');
+					// var button = document.createElement("button");
+					// button.setAttribute('type', 'submit');
+					// button.setAttribute('class', 'btn btn-success');
+					// button.innerHTML = "Login";
+					// forms[i].appendChild(username);
+					// forms[i].appendChild(password);
+					// forms[i].appendChild(button);
+					forms[i].innerHTML = loginform;
 				}else if (forms[i].getAttribute('data-form-type') == "register" || "join" || "signup") {
 					var username = document.createElement("input");
 					username.setAttribute('placeholder', 'Username or Email');
